2c2b71e56e54c5903abb292c735235f09eab1e48
K2P <foxk2p@gmail.com>
2020-06-22 17:53:45 -0700

    model constructed

---
+++ models/Workout.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose')
+
+const workoutSchema = new Schema({
+  day: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  exercises: [{
+    type: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    duration: {
+      type: Number,
+      required: true
+    },
+    weight: {
+      type: Number,
+      required: true
+    },
+    reps: {
+      type: Number,
+      required: true
+    },
+    sets: {
+      type: Number,
+      required: true
+    }
+  }]
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true
+  }
+})
+
+userSchema.virtual('totalWeight').get(function () {
+  return this.items.reduce((t, exercise) => t + exercise.weight, 0)
+})
+
+module.exports = model('Workout', workoutSchema)
